59b421881407d6799dcf50784fe207c80c30a0a3
Julio-Souto <julio.souto.zas@gmail.com>
2023-03-07 14:26:53 +0100

    settings

--- vite-project-nodes/App.js
+++ vite-project-nodes/App.js
@@ -1,9 +1,13 @@
+import layout from "./layout.html/?raw"
+
 export function App () {
   const htmlDom= document.getElementById("app")
   htmlDom.innerHTML = "<h1>Pagina</h1><button>Boton</button>"
 
   const container = document.createElement("main")
+  if(!container) throw new Error("Error")
   container.classList.add("container")
   container.innerHTML = "texto"
+  container.innerHTML = layout
   htmlDom.appendChild(container)
 }
